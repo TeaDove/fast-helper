5b9d77e3b1dcbcc7b9e78c6986adda611a966025
TeaDove <peter.ibragimov@gmail.com>
2021-03-01 15:21:20 +0100

    ssl without docker

--- start_docker_less.sh
+++ start_docker_less.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd src/app
-uvicorn main:app --host localhost --reload --port 8000
+uvicorn main:app --host tesseract.club --reload --port 8000 --ssl-keyfile /etc/letsencrypt/live/tesseract.club/privkey.pem --ssl-certfile /etc/letsencrypt/live/tesseract.club/cert.pem
